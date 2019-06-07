ba997b224881902dc732cdacfaa4caf995a71780
jhon martinez <jhonmartinez.exec@gmail.com>
2019-06-07 15:30:51 +0800

    date

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -6,23 +6,39 @@
         <title>Create</title>
     </head>
     <body>
+    
+        <?php
+            $dateAnnounced = date("Y-m-d");  
+        ?>
         <div class="container">
             <form action="create" method="POST">
         <h2 class="pt-5">New Announcement</h2>
         <hr>
         <a href="/view">View announcements</a>
-                <div class="form-group mt-5">
+
+            <div class="row mt-5">
+            <div class="col col-lg-8">
+                <div class="form-group">
                     <input type="text" class="form-control" name="title" placeholder="Title" value="">
                     <div>{{ $errors->first('title') }}</div>
+                </div>
+            </div>
+
+            <div class="col col-lg-4">
+                <div class="form-group">
+                    <input type="date" name="date" class="form-control" value="{{ $dateAnnounced }}">
+                </div>
+                </div>
+
+            </div>
+
+            
+                <div class="form-group">
                 </div>
                 <div class="form-group">
                     <textarea name="content" id="" cols="30" rows="10" class="form-control" placeholder="What do you want to announce?"></textarea>
                     <div>{{ $errors->first('content') }}</div>
                 </div>
-                <?php
-                    $dateAnnounced = date("Y/m/d");  
-                ?>
-                <input type="text" value="{{ $dateAnnounced }}" style="display:none" name="date">
                 <button type="submit" class="btn btn-success" style="float:right">POST</button>
                 @csrf
             </form>
