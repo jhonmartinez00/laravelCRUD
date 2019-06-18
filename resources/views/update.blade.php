c7f57d1f517ebea749cdc3e4a1981cfb312b4856
jhon martinez <jhonmartinez.exec@gmail.com>
2019-06-18 11:10:26 +0800

    edit

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -1,25 +1,36 @@
 @extends('layouts.app')
 @section('content')
     <body>
+    
         <div class="container">
             <form action="/update/{{ $announcement-> id}}" method="POST">
             @method('PATCH')
         <h2 class="pt-5">Edit Details
-        <a href="/view" class="btn btn-secondary float-right">Back</a>
+        <a h0ref="/view" class="btn btn-secondary float-right">Back</a>
         </h2>
         <hr>
-                <div class="form-group mt-5">
-                    <input type="text" class="form-control" name="title" value="{{ $announcement-> title}}">
+      
+      
+      
+        <div class="row mt-5">
+            <div class="col col-lg-8">
+                <div class="form-group">
+                <input type="text" class="form-control" name="title" value="{{ $announcement-> title}}">
                     <div>{{ $errors->first('title') }}</div>
                 </div>
+            </div>
+
+            <div class="col col-lg-4">
+                <div class="form-group">
+                    <input type="date" name="date" class="form-control" value="{{ $announcement->date }}">
+                </div>
+                </div>
+
+            </div>
                 <div class="form-group">
                     <textarea name="content" id="" cols="30" rows="10" class="form-control">{{ $announcement-> content }}</textarea>
                     <div>{{ $errors->first('content') }}</div>
                 </div>
-                <?php
-                    $dateAnnounced = date("Y/m/d");  
-                ?>
-                <input type="text" value="{{ $dateAnnounced }}" style="display:none" name="date">
                 <button type="submit" class="btn btn-success" style="float:right">SAVE</button>
                 @csrf
             </form>
